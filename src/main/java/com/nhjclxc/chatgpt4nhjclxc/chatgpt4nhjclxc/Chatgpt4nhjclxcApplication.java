04c3218bbe8a39a87f5dc767890e1c5a65b47a2b
罗贤超 <nhjclxc@163.com>
2023-05-21 09:23:21 +0800

    init project

---
+++ src/main/java/com/nhjclxc/chatgpt4nhjclxc/chatgpt4nhjclxc/Chatgpt4nhjclxcApplication.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.chatgpt4nhjclxc.chatgpt4nhjclxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatgpt4nhjclxcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chatgpt4nhjclxcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhjclxc/chatgpt4nhjclxc/chatgpt4nhjclxc/Chatgpt4nhjclxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhjclxc.chatgpt4nhjclxc.chatgpt4nhjclxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chatgpt4nhjclxcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
